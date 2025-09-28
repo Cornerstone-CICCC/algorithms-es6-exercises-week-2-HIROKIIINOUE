713ae7c17ae022d5b07cd6e74e569d8c6288a4f3
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-09-28 11:22:55 -0700

    done ex13.js

--- ex13.js
+++ ex13.js
@@ -9,7 +9,41 @@ Create a function named talkingCalendar that takes in a date string with the for
 */
 
 const talkingCalendar = function (date) {
-  // Your code here
+  const eachData = date.split("/");
+  const months = [
+    "no data",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const year = eachData[0];
+  const month = months[Number(eachData[1])];
+
+  function dayConvert(day) {
+    if (day === 1) {
+      return "1st";
+    } else if (day === 2) {
+      return "2nd";
+    } else if (day === 3) {
+      return "3rd";
+    } else {
+      return `${day}th`;
+    }
+  }
+
+  const day = dayConvert(Number(eachData[2]));
+
+  return `${month} ${day}, ${year}`;
 };
 
 console.log(talkingCalendar("2017/12/02")); // December 2nd, 2017
